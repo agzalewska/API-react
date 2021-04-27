966b039d9bc403b0ad43b36622a0a517958624e7
Agnieszka Zalewska <agnieszka.e.zalewska@gmail.com>
2021-04-27 22:54:32 +0200

    first commit

---
+++ functions/products.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+const Airtable = require('airtable-node');
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('appEVg5VlmweVVsin')
+  .table('products');
+
+exports.handler = async (event, context, cb) => {
+  const { id } = event.queryStringParameters;
+  if (id) {
+    try {
+      const product = await airtable.retrieve(id);
+      if (product.error) {
+        return {
+          statusCode: 404,
+          body: `No product with id: ${id}`,
+        };
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(product),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: `Server Error`,
+      };
+    }
+  }
+  try {
+    const { records } = await airtable.list();
+    const products = records.map((product) => {
+      const { id } = product;
+      const {
+        name,
+        image,
+        price,
+        company,
+        staock,
+        category,
+        shipping,
+        description,
+        colors,
+        reviews,
+        stars,
+      } = product.fields;
+      const url = image[0].url;
+      return {
+        id,
+        name,
+        url,
+        price,
+        company,
+        image,
+        staock,
+        category,
+        shipping,
+        description,
+        colors,
+        reviews,
+        stars,
+      };
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(products),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: 'Server Error',
+    };
+  }
+};
